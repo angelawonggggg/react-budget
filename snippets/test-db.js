261325e109273a70a715f7666ffd3fc489ab2971
chesterip <chesterip0510@gmail.com>
2022-04-24 23:40:29 -0400

    Added mongodb

---
+++ snippets/test-db.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const dbUri = 'mongodb://db:27017'
+
+// Create a new MongoClient
+const client = new MongoClient(dbUri);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
