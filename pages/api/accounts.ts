5b5ee41d47022cc3bd0f33f8eaf5141b87d15fad
Angela <wonglaichu@gmail.com>
2022-05-12 14:13:01 -0400

    Connect to db before query

--- pages/api/accounts.ts
+++ pages/api/accounts.ts
@@ -1,7 +1,9 @@
+import { connect } from "middleware/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import Account from "../../models/accounts";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await connect();
   if (req.method === "POST") {
     const { title, balance } = req.body;
 
@@ -19,7 +21,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   if (req.method === "GET") {
     try {
-      const accounts = await Account.find({}, "-_id -__v");
+      const accounts = await Account.find({});
       res.status(200).json({ success: true, data: accounts });
     } catch (error) {
       res.status(400).json({ success: false, error: error });
